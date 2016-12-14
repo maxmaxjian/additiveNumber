f195967eeec699cc5ca8b4cdf53c5387518a7bfd
Jian Wei <jianwei@jianwei-deb8-64>
2016-12-14 11:23:35 -0500

    first version

--- main.cpp
+++ main.cpp
@@ -1,28 +1,78 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <numeric>
+#include <algorithm>
+#include <iterator>
 
 class solution {
   public:
-    std::vector<std::vector<int>> isAdditiveNumber(const std::string & num) {
-        
+    std::vector<std::vector<std::string>> isAdditiveNumber(const std::string & num) {
+        std::vector<std::vector<std::string>> paths;
+        for (size_t len1 = 1; len1 < num.size()-1; len1++) {
+            for (size_t len2 = len1+1; len2 < num.size(); len2++) {
+                std::vector<std::string> vec;
+                vec.push_back(num.substr(0, len1));
+                vec.push_back(num.substr(len1, len2-len1));
+                auto temp = grow(num, vec);
+                if (!temp.empty() && temp.size() > 2)
+                    paths.push_back(temp);
+            }
+        }
+        std::for_each(paths.begin(), paths.end(),
+                      [](const std::vector<std::string> & v){
+                          std::copy(v.begin(), v.end(), std::ostream_iterator<std::string>(std::cout, " "));
+                          std::cout << std::endl;
+                      });
+
+        return paths;
     }
 
   private:
-    std::vector<std::vector<int>> grow(const std::string & num, const std::vector<int> & curr) {
-        std::vector<std::vector<int>> result;
-        if (std::accumulate(curr.begin(), curr.end(), 0, [](size_t i, const std::vector<int> & v){return i+v.size();}) == nums.size())
-            result.push_back(curr);
+    size_t lenOf(const std::vector<std::string> & vec) {
+        return std::accumulate(vec.begin(), vec.end(), 0,
+                               [](size_t i, const std::string & s){
+                                   return i+s.size();
+                               });
+    }
+    
+    std::vector<std::string> grow(const std::string & num, const std::vector<std::string> & curr) {
+        std::vector<std::string> result;
+        if (lenOf(curr) == num.size())
+            result = curr;
         else {
-            std::vector<int> next = getNext(num, curr);
-            for (auto & nx : next) {
-                
-            }
+     	    std::string next = getNext(num, curr);
+	    if (!next.empty()) {
+            	auto cpy = curr;
+	    	cpy.push_back(next);
+		result = grow(num, cpy);
+	    }
         }
         return result;
     }
-
-    std::vector<int> getNext(const std::string & num, const std::vector<int> & curr) {
-        
+    
+    std::string getNext(const std::string & num, const std::vector<std::string> & curr) {
+        if (curr.size() < 2)
+            return std::string();
+        else {
+            if (lenOf(curr) == num.size())
+                return std::string();
+            else {
+                size_t start = lenOf(curr);
+                int nextint = std::stoi(curr[curr.size()-1])+std::stoi(curr[curr.size()-2]);
+                if (num.find(std::to_string(nextint), start) == start)
+                    return std::to_string(nextint);
+                else
+                    return std::string();
+            }
+        }
     }
 };
+
+int main() {
+    // std::string num{"112358"};
+    std::string num{"199100199"};
+
+    solution soln;
+    auto paths = soln.isAdditiveNumber(num);
+}
